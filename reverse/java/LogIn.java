7207761cefb800596f309a2ef1dea9a1104fe6a6
catalin <catalincojoc@gmail.com>
2019-03-14 09:26:20 +0200

    reverse

---
+++ reverse/java/LogIn.java
@@ -0,0 +1,27 @@
+// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   LogIn.java
+
+import java.util.Vector;
+
+public class LogIn
+{
+
+    public LogIn()
+    {
+    }
+
+    public void setNume(String s)
+    {
+    }
+
+    public String setParola(String nume)
+    {
+        return null;
+    }
+
+    public String nume;
+    public String parola;
+    public Vector myPlayer;
+}
---
+++ reverse/java/NameGenerator.java
@@ -0,0 +1,21 @@
+// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   NameGenerator.java
+
+import java.util.Vector;
+
+public class NameGenerator
+{
+
+    public NameGenerator()
+    {
+    }
+
+    public void setNume(String s)
+    {
+    }
+
+    public String nume;
+    public Vector myPlayer;
+}
